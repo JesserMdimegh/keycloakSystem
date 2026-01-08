3b6d77a3b4cfaf02443afd12b1b10b01d28b7513
JesserMdimegh <jessermdimagh@gmail.com>
2026-01-08 19:06:06 +0100

    initial login

--- frontend/src/components/providers.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextAuthSessionProvider refetchInterval={0}>
-      {children}
-    </NextAuthSessionProvider>;
-}
